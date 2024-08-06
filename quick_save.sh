13f349b3c976d09744e5be323382957ee76e79eb
farzahn <github@farzahn.com>
2024-08-05 21:53:21 -0700

    updated kotor.h

--- quick_save.sh
+++ quick_save.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-git add * && git commit -m "quick save" && git push
 
-git config --global user.email "github@farzahn.com" && git config --global user.name "farzahn"
+# Prompt the user to enter a commit message
+echo "Please enter a commit message:"
+read commit_message
+
+# Add changes to the staging area, commit with the user's message, and push to the repository
+git add * && git commit -m "$commit_message" && git push
+
+
+## git config --global user.email "github@farzahn.com" && git config --global user.name "farzahn"
